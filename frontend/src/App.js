e869885a455be92c0bbbc20ebd3e84a359006011
niniwoo <qhdk6914@naver.com>
2023-11-07 15:51:37 -0800

    created blog post page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import { Provider } from 'react-redux';
-import { store } from './store/configureStore';
-import BlogPostForm from './example/BlogPostForm'; // Example component for blog posting
-import UserProfile from './example/UserProfile'; // Example component for user profile
+import React from "react";
+import { Provider } from "react-redux";
+import { store } from "./store/configureStore";
+import BlogPostPage from "./components/BlogPostPage.jsx"; // Example component for blog posting
+import UserProfile from "./example/UserProfile"; // Example component for user profile
 
 function App() {
   return (
@@ -10,7 +10,7 @@ function App() {
       <div>
         {/* Your application components go here */}
         <UserProfile />
-        <BlogPostForm />
+        <BlogPostPage />
         {/* You can also use Routes from react-router-dom to navigate between these components */}
       </div>
     </Provider>
