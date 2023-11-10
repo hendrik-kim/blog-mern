993a61bc71298a37329cfc807b4a193e71753cfd
hendrik-kim <hank.dohwankim@gmail.com>
2023-11-10 11:43:56 +0900

    add api endpoints fro frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,12 @@
-
-import React from "react";
-import { Provider } from "react-redux";
-import { store } from "./store/configureStore";
-import UserProfile from "./example/UserProfile"; // Example component for user profile
-import AddPostForm from "./components/AddPostForm";
+import React from 'react';
+import { Provider } from 'react-redux';
+import { store } from './store/configureStore';
+import UserProfile from './example/UserProfile'; // Example component for user profile
+import AddPostForm from './components/AddPostForm';
 
 function App() {
   return (
+    // TODO: React Router setup required
     <Provider store={store}>
       <div>
         {/* Your application components go here */}
