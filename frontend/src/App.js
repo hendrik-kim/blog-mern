9da17dcc23e60f832efd1a229e504512b906ecc3
sulgikim <92609054+sulgikim@users.noreply.github.com>
2023-11-10 13:46:25 -0800

    Add Registration Feature

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { store } from './store/configureStore';
+
 import UserProfile from './example/UserProfile'; // Example component for user profile
 import AddPostForm from './components/AddPostForm';
+
+import Register from './components/Register';
+
 
 function App() {
   return (
@@ -10,6 +14,9 @@ function App() {
     <Provider store={store}>
       <div>
         {/* Your application components go here */}
+        <Register />
+        <br />
+        <br />
         <UserProfile />
         <AddPostForm />
       </div>
---
+++ frontend/src/components/Register.jsx
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+import { useAppSelector, useAppDispatch } from '../store/configureStore';
+import { signOut, signUpUser  } from "../slices/accountSlice";
+
+function Register() {
+    const dispatch = useAppDispatch();
+    const userInfo = useAppSelector((state) => state.account.user);
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [username, setUsername] = useState('');
+
+    const handleRegister = () => {
+        //If passwords that user typed in are unmatched, display alert
+        if(password !== confirmPassword) {
+            alert('Password unmatched');
+        } else {
+            dispatch(signUpUser({ email, password, username}));
+        }
+    }
+
+    const handleLogout = () => {
+        dispatch(signOut());
+    };
+
+    return (
+        <div>
+            {/* if user successfully registered, show welcome message with logout button */}
+            {userInfo ? (
+                <div>
+                    <p>{`Welcome, ${userInfo.username}`}</p>
+                    <button onClick={handleLogout}>Logout</button>
+                </div>
+            ) : ( 
+                <div style={{display: 'flex', flexDirection: 'column', width: '30%', gap: 10}}>
+                    <input 
+                        type='text'
+                        placeholder='Email'
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                    <input 
+                        type='password'
+                        placeholder='Password'
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                    <input 
+                        type='password'
+                        placeholder='Confirm Password'
+                        value={confirmPassword}
+                        onChange={(e) => setConfirmPassword(e.target.value)}
+                    />
+                    <input 
+                        type='text'
+                        placeholder='User name'
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                    />
+                    <button onClick={handleRegister}>Register</button>
+                </div>
+            )
+
+
+            }
+
+        </div>
+
+    )
+}
+
+export default Register; 
