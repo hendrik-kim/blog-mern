fdfbb02217ad8a93b30e05801b77ef3e61127a0f
niniwoo <qhdk6914@naver.com>
2023-11-10 20:36:25 -0800

    added asynchronous action

--- frontend/src/slices/postSlice.js
+++ frontend/src/slices/postSlice.js
@@ -1,9 +1,31 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+const simulateApiCall = (data) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(data);
+    }, 1000);
+  });
+};
+
+export const addPostAsync = createAsyncThunk(
+  "posts/addPostAsync",
+  async (post, { dispatch }) => {
+    const response = await simulateApiCall(post);
+    dispatch(postAdded(response));
+  }
+);
 
 const initialState = [
   //example
-  { title: "First title", content: "First content", selectedOption: "public" },
   {
+    status: "idle",
+    title: "First title",
+    content: "First content",
+    selectedOption: "public",
+  },
+  {
+    status: "idle",
     title: "Second title",
     content: "Second content",
     selectedOption: "public",
@@ -21,6 +43,7 @@ const postsSlice = createSlice({
       prepare(title, content, selectedOption) {
         return {
           payload: {
+            status: "idle",
             title,
             content,
             selectedOption,
@@ -28,6 +51,28 @@ const postsSlice = createSlice({
         };
       },
     },
+  },
+  extraReducers: (builder) => {
+    const updatePostStatus = (state, action, status) => {
+      const { title } = action.meta.arg;
+      const post = state.find((post) => post.title === title);
+
+      if (post) {
+        post.status = status;
+      }
+    };
+
+    builder
+      .addCase(addPostAsync.pending, (state, action) => {
+        updatePostStatus(state, action, "Loading");
+      })
+      .addCase(addPostAsync.fulfilled, (state, action) => {
+        updatePostStatus(state, action, "Complete");
+        state.push(action.payload);
+      })
+      .addCase(addPostAsync.rejected, (state, action) => {
+        updatePostStatus(state, action, "Fail");
+      });
   },
 });
 
