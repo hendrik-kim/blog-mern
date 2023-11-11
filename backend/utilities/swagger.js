b7c1b9e19d1a5e550512d7d6b1733bdae09a382f
hendrik-kim <hank.dohwankim@gmail.com>
2023-11-12 04:16:09 +0900

    add component schemas

--- backend/utilities/swagger.js
+++ backend/utilities/swagger.js
@@ -15,6 +15,137 @@ const swaggerDefinition = {
       description: 'Development Server',
     },
   ],
+  components: {
+    schemas: {
+      Post: {
+        type: 'object',
+        properties: {
+          id: {
+            type: 'string',
+            description: 'Unique identifier for the post',
+          },
+          user: {
+            type: 'string',
+            description: 'User ID of the post author',
+          },
+          title: {
+            type: 'string',
+            description: 'Title of the post',
+          },
+          imageUrl: {
+            type: 'string',
+            description: 'Image URL of the post',
+          },
+          category: {
+            type: 'string',
+            description: 'Category of the post',
+          },
+          content: {
+            type: 'string',
+            description: 'Content of the post',
+          },
+          comments: {
+            type: 'array',
+            items: {
+              type: 'string',
+              description: 'Array of comment IDs',
+            },
+          },
+          likes: {
+            type: 'array',
+            items: {
+              $ref: '#/components/schemas/Like',
+            },
+          },
+          numComments: {
+            type: 'number',
+            description: 'Number of comments on the post',
+          },
+          numLikes: {
+            type: 'number',
+            description: 'Number of likes on the post',
+          },
+          numViews: {
+            type: 'number',
+            description: 'Number of views of the post',
+          },
+          isPublic: {
+            type: 'boolean',
+            description: 'Whether the post is public',
+          },
+        },
+      },
+      Like: {
+        type: 'object',
+        properties: {
+          name: {
+            type: 'string',
+            description: 'Name of the user who liked',
+          },
+          like: {
+            type: 'number',
+            description: 'Like count',
+          },
+        },
+      },
+      Comment: {
+        type: 'object',
+        properties: {
+          id: {
+            type: 'string',
+            description: 'Unique identifier for the comment',
+          },
+          postId: {
+            type: 'string',
+            description: 'Post ID that the comment belongs to',
+          },
+          userId: {
+            type: 'string',
+            description: 'User ID of the comment author',
+          },
+          content: {
+            type: 'string',
+            description: 'Content of the comment',
+          },
+          likes: {
+            type: 'array',
+            items: {
+              type: 'string',
+              description: 'Array of user IDs who liked the comment',
+            },
+          },
+          replies: {
+            type: 'array',
+            items: {
+              type: 'string',
+              description: 'Array of reply comment IDs',
+            },
+          },
+        },
+      },
+      User: {
+        type: 'object',
+        properties: {
+          id: {
+            type: 'string',
+            description: 'Unique identifier for the user',
+          },
+          username: {
+            type: 'string',
+            description: 'Username of the user',
+          },
+          email: {
+            type: 'string',
+            description: 'Email address of the user',
+          },
+          isAdmin: {
+            type: 'boolean',
+            description: 'Whether the user is an admin',
+          },
+        },
+      },
+    },
+  },
 };
 
 const options = {
