1c4d5b90a3654bb13e913a0ae3079cfe49d5d1c1
Hendrik Kim <48870515+hendrik-kim@users.noreply.github.com>
2023-11-16 09:23:29 +0900

    Remove sensitive information

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,12 +4,8 @@ import { Provider } from 'react-redux';
 import { store } from './store/configureStore';
 import { validateUserSession } from './slices/accountSlice';
 import RootLayout from './layouts/RootLayout';
-import { GoogleOAuthProvider } from '@react-oauth/google';
 
 function App() {
-  console.log(process.env.GOOGLE_CLIENT_ID);
-  const clientID =
-    '1031592030135-2c9fabeh56q6vuklpn68jknf48ie8fai.apps.googleusercontent.com';
   const dispatch = useAppDispatch();
   const isAuthenticated = useAppSelector(
     (state) => state.account.isAuthenticated
@@ -21,12 +17,7 @@ function App() {
 
   return (
     <Provider store={store}>
-      <GoogleOAuthProvider
-        eOAuthProvider
-        clientId='1031592030135-2c9fabeh56q6vuklpn68jknf48ie8fai.apps.googleusercontent.com'
-      >
         <RootLayout />
-      </GoogleOAuthProvider>
     </Provider>
   );
 }
