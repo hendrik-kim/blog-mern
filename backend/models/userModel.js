6eab4fc0bdcb193c400bc73eae6c32519897e60a
sulgikim <92609054+sulgikim@users.noreply.github.com>
2024-01-05 00:41:52 -0800

    Change username to be unique

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -7,6 +7,7 @@ const userSchema = mongoose.Schema(
     username: {
       type: String,
       required: [true, "Username is required"],
+      unique: true,
     },
     email: {
       type: String,
