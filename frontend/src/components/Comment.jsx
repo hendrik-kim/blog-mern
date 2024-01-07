1f694adac67cff8ffab891b06b45c24a327ba7d5
niniwoo <qhdk6914@naver.com>
2024-01-06 23:22:58 -0800

    display posting and redirect to postin detail after click on Search componenet

---
+++ frontend/src/components/Comment.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Comment = () => {
+  //TODO: save userinfo: username userid , posting info: posting id
+  //TODO: should we create comment table?
+  return (
+    <div>
+      <input placeholder="write the comment" />
+      <button>submit</button>
+    </div>
+  );
+};
+
+export default Comment;
